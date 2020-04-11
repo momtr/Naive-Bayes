2104bde39d961e0ca276bc7637fe4f5c1ac0bfe2
Moritz Mitterdorfer <47625175+moritzmitterdorfer@users.noreply.github.com>
2020-04-12 00:18:53 +0200

    comments

--- src/naiveBayes.js
+++ src/naiveBayes.js
@@ -1,40 +1,62 @@
-class NaiveBayes {
+// Naive Bayes
+// Moritz Mitterdorfer, 2020
 
+/**
+ * Implementation of the  Naive Bayes Classifier
+ */
+class NaiveBayesClassifier {
+
+    /**
+     * Constructor
+     * Data Object must have:
+     *  - data.classes: string array of all classes
+     *  - data.features: string array of all feature names
+     *  - data.training_data: array containing objects:
+     *      - object.class: class name as string
+     *      - object.feature_1: value_1 (number)
+     * @param {object} data - data object
+     */
     constructor(data) {
         this.classes = data.classes;
         this.features = data.features;
         this.data = data.training_data;
         this.formatted = {};
-        for(let i of this.classes) {
+        for (let i of this.classes) {
             // has to be done each time, otherwise it would create references!
             let obj = {};
-            for(let j of this.features) {
+            for (let j of this.features) {
                 obj[j] = [];
             }
             this.formatted[i] = obj;
         }
-        for(let i of this.data) {
-            for(let j of this.features) {
+        for (let i of this.data) {
+            for (let j of this.features) {
                 this.formatted[i.class][j].push(i[j]);
             }
         }
+        // n is the number of all training examples
         this.n = data.training_data.length;
+        // this object stores the number of training examples belonging to a certain class
         this.countClasses = {};
-        for(let i of this.classes) {
+        for (let i of this.classes) {
             // TODO: now, we are assuming no feature is undefined!
             this.countClasses[i] = this.formatted[i][this.features[0]].length;
         }
-        // train
+
+        // training
         // (1) P(Class) = N(all) / count(Class)
+        // the propability for each class is claculated and stored in an object
         this.propabilities = {};
         // (2) P(Feature|Class) -> distribution
+        // the propability for a feature belonging to a certain class is calculated using the gaussian bell curve
+        // for that we need the mean and the standard deviation
         this.statistics = {};
-        for(let i of this.classes) {
-            // 1
+        for (let i of this.classes) {
+            // set one
             this.propabilities[i] = this.countClasses[i] / this.n;
-            // 2
+            // set two
             this.statistics[i] = {};
-            for(let j of this.features) {
+            for (let j of this.features) {
                 this.statistics[i][j] = {};
                 let m = this.mean(this.formatted[i][j]);
                 let sd = this.standardDeviation(this.formatted[i][j], m)
@@ -44,52 +66,69 @@ class NaiveBayes {
         }
     }
 
+    /**
+     * Returns the mean of an array
+     * @param {array} array - array containing numbers 
+     */
     mean(array) {
         let sum = 0;
-        for(let i of array) {
+        for (let i of array) {
             sum += i;
         }
         return sum / array.length;
     }
 
+    /**
+     * returns the standard deviation of an array
+     * @param {array} array -  array containing numbers
+     * @param {number} m - mean can be specified, it not, it is calculated
+     */
     standardDeviation(array, m) {
-        if(!m)
+        if (!m)
             m = this.mean(array);
         let n = array.length;
         let sum = 0;
-        for(let i of array) {
+        for (let i of array) {
             sum += (i - m) * (i - m);
         }
-        return Math.pow((sum / n), (1/2));
+        return Math.pow((sum / n), (1 / 2));
     }
 
-
+    /**
+     * Returns the propability for a feature being part of a class
+     * @param {object} feature_object - object having all features stored: object.feature_1 = value_1
+     */
     predict(feature_object) {
-        // we get a feature array and map it to a class
+        // class array
         let ret = [];
-        for(let i of this.classes) {
+        // go through all classes
+        for (let i of this.classes) {
             // P = P(Class) * P(Feature|Class) = 
             //   = P(Class) * P(F_1|Class) * P(F_n|Class)
+            // get the prop. for that class
             let p = this.propabilities[i];
-            for(let j of this.features) {
-                // calculate propb. with gaussian bell curve
+            // go through all features
+            for (let j of this.features) {
+                // calculate prop. with gaussian bell curve
                 let sd = this.statistics[i][j].sd;
-                let gauss_1 = 1 / (sd * Math.pow(2 * Math.PI, (1/2)));
+                let gauss_1 = 1 / (sd * Math.pow(2 * Math.PI, (1 / 2)));
                 let x = feature_object[j];
                 let m = this.statistics[i][j].mean;
-                let gauss_2 = Math.exp(-(1/2) * Math.pow((x - m) / sd, 2));
-                // console.log((gauss_1 * gauss_2));
+                let gauss_2 = Math.exp(-(1 / 2) * Math.pow((x - m) / sd, 2));
                 p *= (gauss_1 * gauss_2);
             }
+            // add class and propability to array
             ret.push({
                 class: i,
                 propability: p
             });
         }
+        // sort the array (max: array[0])
         ret.sort((a, b) => {
-            if(a.propability < b.propability) return 1;
-            else                              return -1;
+            if (a.propability < b.propability) return 1;
+            else return -1;
         });
+        // finally, return the array containing classes and their propabilities
         return ret;
     }
 
--- src/script.js
+++ src/script.js
@@ -1,55 +1,69 @@
+// object storing all classes, features and training examples
 let data = {};
+// varaible storing the Naive Bayes Classifier Instance
 let classifier;
+// class 'a' is pre-selected
 let currentClass = 'a';
 
+// called at the beginning
 function setup() {
-
+    // specifie classes (a, b, c)
     data.classes = ["a", "b", "c"];
+    // specifie features (x-, y-coordinates)
     data.features = ["x", "y"];
     data.training_data = [];
-
     // DOM
     createCanvas(600, 600);
     noStroke();
-    
-    
 }
 
+// called every tick
 function draw() {
-
+    // set background color to rgb(40,40,40)
     background(40);
 
-    if(classifier) {
-        for(let y = 0; y < height; y += 20) {
-            for(let x = 0; x < width; x += 20) {
+    // if the classifier instance has been created
+    if (classifier) {
+        // go through all 20x20 squares of the canvas
+        for (let y = 0; y < height; y += 20) {
+            for (let x = 0; x < width; x += 20) {
+                // predict class using the Naive Bayes Classifier instance
                 let result = classifier.predict({ x, y });
+                // get the class name
                 let label = result[0].class;
-                if(label == 'a')        fill(255, 0, 0, 50);
-                else if(label == 'b')   fill(0, 255, 0, 50);
-                else if(label == 'c')   fill(0, 0, 255, 50);
+                // specifie color according to class 
+                if (label == 'a') fill(255, 0, 0, 50);
+                else if (label == 'b') fill(0, 255, 0, 50);
+                else if (label == 'c') fill(0, 0, 255, 50);
+                // draw a rect
                 rect(x, y, 20, 20);
             }
         }
     }
 
-    for(let i of data.training_data) {
-        if(i.class == 'a')      fill(255, 0, 0);
-        else if(i.class == 'b') fill(0, 255, 0);
-        else if(i.class == 'c') fill(0, 0, 255);
+    // draw all points that have been added as training examples
+    for (let i of data.training_data) {
+        if (i.class == 'a') fill(255, 0, 0);
+        else if (i.class == 'b') fill(0, 255, 0);
+        else if (i.class == 'c') fill(0, 0, 255);
         ellipse(i.x, i.y, 30, 30);
     }
 
 }
 
+// if a key has been pressed
 function keyPressed() {
-    if(key == 'a' || key == 'b' || key == 'c') {
+    if (key == 'a' || key == 'b' || key == 'c') {
+        // change the current class to the key's value
         currentClass = key;
     }
-    else if(key == 's') {
-        classifier = new NaiveBayes(data);
+    else if (key == 's') {
+        // if 's' has been pressed, create a Naive Bayes Classifier instance
+        classifier = new NaiveBayesClassifier(data);
     }
 }
 
+// if the mouse has been pressed, add a new training example 
 function mousePressed() {
     data.training_data.push({
         class: currentClass,
